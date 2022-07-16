2cb7885b0885d0a1ba73bca0e10886f07626275d
darryI <darryl.thatguy.13@gmail.com>
2022-07-16 00:26:46 -0400

    Added delete API (not working because of route).

--- frontend/src/utils/controller.js
+++ frontend/src/utils/controller.js
@@ -4,3 +4,7 @@ import { API_BASE } from "../constants";
 export function getApiData(url) {
   return axios.get(API_BASE + url).then((response) => response.data);
 }
+
+export function deleteApiData(url) {
+  return axios.delete(API_BASE + url).then((response) => response.data);
+}
