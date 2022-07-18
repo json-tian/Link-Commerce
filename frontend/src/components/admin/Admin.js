dd0b00225094c6b7130ab50a2c27b497a4b2944f
darryI <darryl.thatguy.13@gmail.com>
2022-07-18 16:28:07 -0400

    Modal for adding products (no API call yet).

--- frontend/src/components/admin/Admin.js
+++ frontend/src/components/admin/Admin.js
@@ -15,6 +15,9 @@ import {
   Button,
   ButtonGroup,
   DisplayText,
+  DropZone,
+  Frame,
+  Modal,
   Page,
   PageActions,
   TextField,
@@ -38,40 +41,46 @@ function Admin({ user }) {
   const [shopName, setShopName] = useState("");
   const [shopDescription, setShopDescription] = useState("");
   const [shopSubpage, setShopSubpage] = useState("");
+
+  const [addProductActive, setAddProductActive] = useState(true);
+
+  const [addProductName, setAddProductName] = useState("");
+  const [addProductDescription, setAddProductDescription] = useState("");
+  const [addProductPrice, setAddProductPrice] = useState("");
+
   const [adminUser, setAdminUser] = useState(user);
 
-  const handleShopNameChange = useCallback(
-    (newValue) => setShopName(newValue),
-    []
+  const handleShopName = useCallback((name) => setShopName(name), [shopName]);
+  const handleShopDescription = useCallback(
+    (description) => setShopDescription(description),
+    [shopDescription]
   );
-  const handleShopDescriptionChange = useCallback(
-    (newValue) => setShopDescription(newValue),
-    []
-  );
-  const handleShopSubpageChange = useCallback(
-    (newValue) => setShopSubpage(newValue),
-    []
-  );
+  const handleShopSubpage = useCallback((subpage) => setShopSubpage(subpage), [
+    shopSubpage,
+  ]);
 
-  const handleEditShopName = useCallback(() => setDisabledShopName(false), []);
+  const handleEditShopName = useCallback(() => setDisabledShopName(false), [
+    disabledShopName,
+  ]);
   const handleEditShopDescription = useCallback(
     () => setDisabledShopDescription(false),
-    []
+    [disabledShopDescription]
   );
   const handleEditShopSubpage = useCallback(
     () => setDisabledShopSubpage(false),
-    []
+    [disabledShopSubpage]
   );
 
-  const saveShopName = useCallback(() => setDisabledShopName(true), []);
+  const saveShopName = useCallback(() => setDisabledShopName(true), [
+    disabledShopName,
+  ]);
   const saveShopDescription = useCallback(
     () => setDisabledShopDescription(true),
-    []
+    [disabledShopDescription]
   );
-  const saveShopSubpage = useCallback(
-    () => setDisabledShopSubpage(true),
-    []
-  );
+  const saveShopSubpage = useCallback(() => setDisabledShopSubpage(true), [
+    disabledShopSubpage,
+  ]);
 
   const handleSaveShopName = () => {
     patchApiData("shops/" + shopData.id, { name: shopName });
@@ -86,7 +95,24 @@ function Admin({ user }) {
     saveShopSubpage();
   };
 
-  const handleDelete = (url) => {
+  const handleAddProductModal = useCallback(
+    () => setAddProductActive(!addProductActive),
+    [addProductActive]
+  );
+
+  const handleAddProductName = useCallback((name) => setAddProductName(name), [
+    addProductName,
+  ]);
+  const handleAddProductDescription = useCallback(
+    (description) => setAddProductDescription(description),
+    [addProductDescription]
+  );
+  const handleAddProductPrice = useCallback(
+    (price) => setAddProductPrice(price),
+    [addProductPrice]
+  );
+
+  const handleDeleteProduct = (url) => {
     deleteApiData(url);
     refreshPage();
   };
@@ -112,69 +138,110 @@ function Admin({ user }) {
     return getData();
   }, [shopData]);
 
-  return (
-    <React.Fragment>
-      {user && user === shopData.email ? (
-        <Page
-          breadcrumbs={[
-            { content: shopData.name, url: "/" + shopData.subpage },
-          ]}
-          title="Admin Page"
-          subtitle={shopData.name}
-          compactTitle
-          primaryAction={<Logout />}
-        >
-          <DisplayText size="extraLarge">Welcome to the Admin Page</DisplayText>
-          <DisplayText size="large">Store Details:</DisplayText>
-          <TextField
-            label="Shop Name"
-            disabled={disabledShopName}
-            value={shopName}
-            onChange={handleShopNameChange}
-            autoComplete="off"
-            connectedRight={
-              <ButtonGroup>
-                <Button onClick={handleEditShopName}>Edit</Button>
-                <Button primary onClick={handleSaveShopName}>
-                  Save
-                </Button>
-              </ButtonGroup>
-            }
-          />
-          <TextField
-            label="Shop Description"
-            disabled={disabledShopDescription}
-            value={shopDescription}
-            onChange={handleShopDescriptionChange}
-            autoComplete="off"
-            connectedRight={
-              <ButtonGroup>
-                <Button onClick={handleEditShopDescription}>Edit</Button>
-                <Button primary onClick={handleSaveShopDescription}>
-                  Save
-                </Button>
-              </ButtonGroup>
-            }
-          />
-          <TextField
-            label="Shop Subpage"
-            disabled={disabledShopSubpage}
-            value={shopSubpage}
-            onChange={handleShopSubpageChange}
-            autoComplete="off"
-            connectedRight={
-              <ButtonGroup>
-                <Button onClick={handleEditShopSubpage}>Edit</Button>
-                <Button primary onClick={handleSaveShopSubpage}>
-                  Save
-                </Button>
-              </ButtonGroup>
-            }
-          />
-          <DisplayText size="large">Products in your store:</DisplayText>
-          <Products products={products} deleteItem={handleDelete} />
+  const addProductButton = (
+    <Button onClick={handleAddProductModal}>Add product</Button>
+  );
 
-          {/* <PageActions
+  const addProductModal = (
+    // <div style={{ height: "50%" }}>
+    <Modal
+      large
+      noScroll
+      open={addProductActive}
+      onClose={handleAddProductModal}
+      title="Add a new product to your shop!"
+      primaryAction={{
+        content: "Add product",
+        onAction: handleAddProductModal,
+      }}
+    >
+      <Modal.Section>
+        <TextField
+          label="Product Name"
+          value={addProductName}
+          onChange={handleAddProductName}
+        ></TextField>
+        <TextField
+          label="Description"
+          value={addProductDescription}
+          onChange={handleAddProductDescription}
+        ></TextField>
+        <TextField
+          label="Price"
+          type="number"
+          prefix="$"
+          value={addProductPrice}
+          onChange={handleAddProductPrice}
+        ></TextField>
+        <DropZone label="Example Image" type="file" onDrop={() => {}}>
+          <DropZone.FileUpload />
+        </DropZone>
+      </Modal.Section>
+    </Modal>
+    // </div>
+  );
+
+  const actualPageMarkup = (
+    <Page
+      breadcrumbs={[{ content: shopName, url: "/" + shopSubpage }]}
+      title="Admin Page"
+      subtitle={shopData.name}
+      compactTitle
+      primaryAction={<Logout />}
+      secondaryActions={addProductButton}
+    >
+      {addProductModal}
+      <DisplayText size="extraLarge">Welcome to the Admin Page</DisplayText>
+      <DisplayText size="large">Store Details:</DisplayText>
+      <TextField
+        label="Shop Name"
+        disabled={disabledShopName}
+        value={shopName}
+        onChange={handleShopName}
+        autoComplete="off"
+        connectedRight={
+          <ButtonGroup>
+            <Button onClick={handleEditShopName}>Edit</Button>
+            <Button primary onClick={handleSaveShopName}>
+              Save
+            </Button>
+          </ButtonGroup>
+        }
+      />
+      <TextField
+        label="Shop Description"
+        disabled={disabledShopDescription}
+        value={shopDescription}
+        onChange={handleShopDescription}
+        autoComplete="off"
+        connectedRight={
+          <ButtonGroup>
+            <Button onClick={handleEditShopDescription}>Edit</Button>
+            <Button primary onClick={handleSaveShopDescription}>
+              Save
+            </Button>
+          </ButtonGroup>
+        }
+      />
+      <TextField
+        label="Shop Subpage"
+        disabled={disabledShopSubpage}
+        value={shopSubpage}
+        onChange={handleShopSubpage}
+        autoComplete="off"
+        connectedRight={
+          <ButtonGroup>
+            <Button onClick={handleEditShopSubpage}>Edit</Button>
+            <Button primary onClick={handleSaveShopSubpage}>
+              Save
+            </Button>
+          </ButtonGroup>
+        }
+      />
+      <DisplayText size="large">Products in your store:</DisplayText>
+      <Products products={products} deleteItem={handleDeleteProduct} />
+
+      {/* <PageActions
       primaryAction={{
         content: "Save Changes",
       }}
@@ -185,7 +252,13 @@ function Admin({ user }) {
         },
       ]}
     /> */}
-        </Page>
+    </Page>
+  );
+
+  return (
+    <React.Fragment>
+      {user && user === shopData.email ? (
+        <Frame>{actualPageMarkup}</Frame>
       ) : (
         <NotAuthorized />
       )}
