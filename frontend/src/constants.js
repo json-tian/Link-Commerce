c4177f09bbaade2416188c1c866313f48baba31e
Jason Tian <tianjason6@gmail.com>
2022-07-25 21:12:42 -0400

    Split env files into development and production

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,1 @@
-export const API_BASE = "https://www.linkcommerce.ca/link-commerce";
-// export const API_BASE = "http://localhost:3000";         FOR LOCAL DEVELOPMENT
+export const API_BASE = process.env.REACT_APP_BASE_URL;
